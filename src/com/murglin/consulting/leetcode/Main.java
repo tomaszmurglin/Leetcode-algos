fa0a2b4030d18cfedc2fc03176d4204aeb287921
Tomasz <tomasz.murglin@acoustic.co>
2020-10-26 15:46:15 +0100

    package renamed

--- src/com/murglin/consulting/leetcode/Main.java
+++ src/com/murglin/consulting/leetcode/Main.java
@@ -1,10 +1,9 @@
-package com.murglin.consulting;
+package com.murglin.consulting.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
